afec38cf37061969a75da3840abf500938d09e75
paulaca <paulaca.a8@gmail.com>
2017-11-11 10:43:27 -0500

    prueba con mi nombre

--- Pruebagit/src/co/com/prueba/Prueba.java
+++ Pruebagit/src/co/com/prueba/Prueba.java
@@ -1,10 +1,14 @@
 package co.com.prueba;
 
 public class Prueba {
+	
+	private String paula="paula";
+
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+    	
+    	
     }
 
 }
