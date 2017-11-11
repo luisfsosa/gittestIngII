7011475aa83fb6e81c8d8b0dd19b2125a7cbbf03
Miguel <migu3l1991@hotmail.com>
2017-11-11 10:59:39 -0500

    Variable Miguel

--- Pruebagit/src/co/com/prueba/Prueba.java
+++ Pruebagit/src/co/com/prueba/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 	
 	private String paula="paula";
 	
-	
+	String MIGUEL;
     
     private String felipe="felipe";
 
@@ -12,7 +12,7 @@ public class Prueba {
 	public String mata= "Alejandro";
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-    	String MIGUEL;
+    	
     }
 
     
