86a2db1e643e79a17d079b948a1b7661840b34a6
Luis Felipe Sosa <luisfsosa@gmail.com>
2017-11-11 10:57:23 -0500

    Auto stash before merge of "master" and "origin/master"
    Logre integrarme con el cambio de paula

--- Pruebagit/src/co/com/prueba/Prueba.java
+++ Pruebagit/src/co/com/prueba/Prueba.java
@@ -5,12 +5,13 @@ public class Prueba {
 	private String paula="paula";
 	
 	
+    
+    private String felipe="felipe";
 
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-    	
-    	
+
     }
 
 }
