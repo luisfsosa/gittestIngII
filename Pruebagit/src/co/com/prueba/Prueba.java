8aad64e78976b8514fb04eb3698747d3726ae86f
Alejandro <alejito.matallana@gmail.com>
2017-11-11 10:42:33 -0500

    Variable nueva

--- Pruebagit/src/co/com/prueba/Prueba.java
+++ Pruebagit/src/co/com/prueba/Prueba.java
@@ -2,9 +2,12 @@ package co.com.prueba;
 
 public class Prueba {
 
+	public String mata= "Alejandro";
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
     }
 
+    
+    
 }
